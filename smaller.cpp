5fca761569814bd91b7a6310cfff4d996270ab5c
Aaleia <aaleia.fernando@gmail.com>
2023-01-30 10:31:14 -0500

    Finished smaller.cpp

--- smaller.cpp
+++ smaller.cpp
@@ -1,1 +1,21 @@
 // Lab 1A
+
+#include <iostream>
+
+int main(){
+  int first;
+  int second;
+  std::cout << "Enter the first number: ";
+  std::cin >> first;
+  std::cout << "Enter the second number: ";
+  std::cin >> second;
+  if (first > second){
+    std::cout << "The smaller of the two is " << second << std::endl;
+  }
+  if (second > first){
+    std::cout << "The smaller of the two is " << first << std::endl;
+  }
+  if (first == second){
+    std::cout << "The numbers are equal." << std::endl;
+   }
+}
